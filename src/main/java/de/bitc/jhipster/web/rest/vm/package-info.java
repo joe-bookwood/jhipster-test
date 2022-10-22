4a6c96f9d050457c83086f5ec9b0fb3f2fd1af67
joe-bookwood <github@buchholz-it.de>
2022-10-22 14:36:32 +0200

    Generated with JHipster 7.9.2

---
+++ src/main/java/de/bitc/jhipster/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package de.bitc.jhipster.web.rest.vm;
---
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'region',
+        data: { pageTitle: 'jhipsterTestApp.region.home.title' },
+        loadChildren: () => import('./region/region.module').then(m => m.RegionModule),
+      },
+      {
+        path: 'country',
+        data: { pageTitle: 'jhipsterTestApp.country.home.title' },
+        loadChildren: () => import('./country/country.module').then(m => m.CountryModule),
+      },
+      {
+        path: 'location',
+        data: { pageTitle: 'jhipsterTestApp.location.home.title' },
+        loadChildren: () => import('./location/location.module').then(m => m.LocationModule),
+      },
+      {
+        path: 'department',
+        data: { pageTitle: 'jhipsterTestApp.department.home.title' },
+        loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule),
+      },
+      {
+        path: 'task',
+        data: { pageTitle: 'jhipsterTestApp.task.home.title' },
+        loadChildren: () => import('./task/task.module').then(m => m.TaskModule),
+      },
+      {
+        path: 'employee',
+        data: { pageTitle: 'jhipsterTestApp.employee.home.title' },
+        loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+      },
+      {
+        path: 'job',
+        data: { pageTitle: 'jhipsterTestApp.job.home.title' },
+        loadChildren: () => import('./job/job.module').then(m => m.JobModule),
+      },
+      {
+        path: 'job-history',
+        data: { pageTitle: 'jhipsterTestApp.jobHistory.home.title' },
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.JobHistoryModule),
+      },
+      /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+    ]),
+  ],
+})
+export class EntityRoutingModule {}
