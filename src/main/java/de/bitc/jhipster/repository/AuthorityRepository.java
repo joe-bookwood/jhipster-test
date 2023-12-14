e1840fe9eda93aadb4b49d731376491a8698eb3c
joe-bookwood <github@buchholz-it.de>
2023-12-14 21:00:33 +0100

    Generated with JHipster 7.9.2

---
+++ src/main/java/de/bitc/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package de.bitc.jhipster.repository;
+
+import de.bitc.jhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
